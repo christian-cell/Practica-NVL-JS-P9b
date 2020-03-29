5bd7a2900eb0a77fcf4d47ad27dfd0a83610196c
Christian-Cell <cristohp74@gmail.com>
2020-03-29 14:14:21 +0200

    practica de matrices 2

---
+++ archivo.js
@@ -0,0 +1,3 @@
+//Destinos de la gira de conciertos de esta semana
+var conciertos = ["Lunes,Madrid","Martes,Sevilla","Miercoles,Barcelona","Jueves,Valencia","viernes,Zaragoza","Sabado,Bilbao","Domingo,Donosti"];
+console.log(conciertos);
